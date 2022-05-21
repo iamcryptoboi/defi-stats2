ef82cc2d058f2545e79ec99fe79f11f285382c96
iamcryptoboi <iamcryptoboi@gmail.com>
2022-05-20 20:19:17 -0400

    Add moralis SDK

--- src/index.js
+++ src/index.js
@@ -3,11 +3,37 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MoralisProvider } from "react-moralis";
+
+const APP_ID = process.env.REACT_APP_MORALIS_APPLICATION_ID;
+const SERVER_URL = process.env.REACT_APP_MORALIS_SERVER_URL;
+
+const Application = () => {
+  const isServerInfo = APP_ID && SERVER_URL ? true : false;
+  //Validate
+  if (!APP_ID || !SERVER_URL)
+    throw new Error(
+      "Missing Moralis Application ID or Server URL. Make sure to set your .env file.",
+    );
+  if (isServerInfo)
+    return (
+      <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
+        <App isServerInfo />
+      </MoralisProvider>
+    );
+  else {
+    return (
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <App />
+      </div>
+    );
+  }
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Application />
   </React.StrictMode>
 );
 
